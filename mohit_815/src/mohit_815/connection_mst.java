3d85160a35f76a8db507f0f652fe464cb9b40e3d
Mohit <Mohit@DESKTOP-2S11U6C>
2024-02-09 11:55:24 +0530

    main function is changed

--- mohit_815/src/mohit_815/connection_mst.java
+++ mohit_815/src/mohit_815/connection_mst.java
@@ -6,12 +6,13 @@ import java.sql.SQLException;
 
 public class connection_mst {
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+	public Connection getConnection() throws SQLException, ClassNotFoundException {
 		// TODO Auto-generated method stub
          Connection con=null;
          Class.forName("com.mysql.cj.jdbc.Driver");
          con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mohit_815", "root", "Mohitkd@9896480943");
          System.out.println("connection established");
+		return con;
 	}
 
 }
